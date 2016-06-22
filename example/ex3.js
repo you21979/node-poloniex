7e009b756d53262cadac5ca5230a5c45cc1c8d35
you21979 <you2197901@gmail.com>
2016-06-22 19:58:41 +0900

    mod

--- example/ex3.js
+++ example/ex3.js
@@ -1,7 +1,7 @@
 var poloniex = require('..')
 var fs = require('fs');
 
-var config = JSON.parse(fs.readFileSync("./account.json"));
+var config = JSON.parse(fs.readFileSync("./account.json", "utf8"));
 
 var api = poloniex.createPrivateApi(config.APIKEY, config.SECRET, "")
 api.marginAccountSummary().then(function(res){
